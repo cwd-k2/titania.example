2bc8bc71485a5b9daecdc70d7cc954e10442b9af
cwd-k2 <kaito.augmented7th@gmail.com>
2020-11-07 00:50:32 +0900

    for titania.go v0.2.2

--- example_03/test_method.rb
+++ example_03/test_method.rb
@@ -1,9 +1,7 @@
 # output, input, answer can be separated by null charactor.
-output = gets "\0"
-input  = gets "\0"
-answer = gets "\0"
+output, _, answer = gets(nil).split("\0")
 
-# this is awful, reversing PASS and FAIL
+# PASS and FAIL
 # and test method should output PASS or FAIL
 STDOUT.puts output == answer ? "FAIL" : "PASS"
 STDERR.puts "Just for fun, inversing the result."
